2375bca1a62094d578e0575628939c87feecf150
pratik karelia <pratik25h.k@gmail.com>
2021-06-21 23:49:36 +0530

    design done

---
+++ script.js
@@ -0,0 +1,10 @@
+const clear = document.getElementById('clear');
+const screen = document.getElementById('screen')
+
+function clearScreen(){
+    screen.value="";
+}
+
+function liveScreen(button){
+    
+}
