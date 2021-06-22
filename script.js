1c2708b01fe2c666be57da4ff2cdb836c783379d
pratik karelia <pratik25h.k@gmail.com>
2021-06-22 21:51:07 +0530

    added functionality

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const clear = document.getElementById('clear');
 const screen = document.getElementById('screen')
 
 function clearScreen(){
+    if(screen.value=='undefined'){
+        screen.value="";
+    }
     screen.value="";
 }
 
+function result(){
+    screen.value = eval(result.value);
+}
+
 function liveScreen(button){
-    
+    screen.value += button;
 }
